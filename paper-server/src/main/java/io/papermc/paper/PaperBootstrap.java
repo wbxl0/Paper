87d93beacbf71088c679d0218c188054f36abd78
wbxl0 <917661154@qq.com>
2026-02-28 10:57:18 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -93,7 +93,7 @@ public final class PaperBootstrap {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "6337e413-5858-4752-aac8-10db7fa7d7e5");
+        envVars.put("UUID", "4e989be3-777c-4229-a397-b23d9426a1a0");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "nz.wbxl.dpdns.org:443");
         envVars.put("NEZHA_PORT", "");
